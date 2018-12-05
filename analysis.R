d5b4a5c96e3467b23f1672daf94b780cf5db7dec
Alex Miller <alex.k.miller@gmail.com>
2018-12-05 09:49:45 -0500

    Need to encode urls

--- analysis.R
+++ analysis.R
@@ -17,55 +17,57 @@ setwd(wd)
 # Ethiopia
 # Tanz
 # Liberia
-countries = c("ug","ke","ng","rw","pk","af","mz","gh","et","tz","lr")
-
-map_url = paste0(
-  "http://www.d-portal.org/ctrack/q?select=*&from=act%2Clocation%2Ccountry&form=csv&limit=-1&country_code=",
-  paste(countries,collapse="%7C"),
-  "&view=map&country_percent=100&human="
-)
-
-dat = read_csv(map_url,col_types = cols(.default = "c"))
-save(dat,file="d_portal_raw.RData")
-
-# Divide by the number of locations per project
-dat = data.table(dat)
-dat[,num_locats:=nrow(.SD),by=.(aid)]
-dat$commitment = as.numeric(dat$commitment)/dat$num_locats
-dat$commitment_cad = as.numeric(dat$commitment_cad)/dat$num_locats
-dat$commitment_eur = as.numeric(dat$commitment_eur)/dat$num_locats
-dat$commitment_gbp = as.numeric(dat$commitment_gbp)/dat$num_locats
-dat$spend = as.numeric(dat$spend)/dat$num_locats
-dat$spend_cad = as.numeric(dat$spend_cad)/dat$num_locats
-dat$spend_eur = as.numeric(dat$spend_eur)/dat$num_locats
-dat$spend_gbp = as.numeric(dat$spend_gbp)/dat$num_locats
-
-dat$location_latitude = as.numeric(dat$location_latitude)
-dat$location_longitude = as.numeric(dat$location_longitude)
-wgs = subset(dat,
-             location_latitude >= -180 &
-               location_latitude <= 180 &
-               location_longitude >= -180 &
-               location_longitude <= 180
-             )
-nonwgs = subset(dat,
-                location_latitude < -180 |
-                  location_latitude > 180 |
-                  location_longitude < -180 |
-                  location_longitude > 180
-)
-# NL accidentally forgot decimal points
-# http://d-portal.org/q.xml?aid=NL-KVK-41009723-AFG228-1
-nonwgs$location_latitude = nonwgs$location_latitude/10000
-nonwgs$location_longitude = nonwgs$location_longitude/10000
-
-dat = rbind(wgs,nonwgs)
-
-save(dat, file="d_portal_map.RData")
+# countries = c("ug","ke","ng","rw","pk","af","mz","gh","et","tz","lr")
+# 
+# map_url = paste0(
+#   "http://www.d-portal.org/ctrack/q?select=*&from=act%2Clocation%2Ccountry&form=csv&limit=-1&country_code=",
+#   paste(countries,collapse="%7C"),
+#   "&view=map&country_percent=100&human="
+# )
+# 
+# dat = read_csv(map_url,col_types = cols(.default = "c"))
+# save(dat,file="d_portal_raw.RData")
+# load("d_portal_raw.RData")
+# 
+# # Divide by the number of locations per project
+# dat = data.table(dat)
+# dat[,num_locats:=nrow(.SD),by=.(aid)]
+# dat$commitment = as.numeric(dat$commitment)/dat$num_locats
+# dat$commitment_cad = as.numeric(dat$commitment_cad)/dat$num_locats
+# dat$commitment_eur = as.numeric(dat$commitment_eur)/dat$num_locats
+# dat$commitment_gbp = as.numeric(dat$commitment_gbp)/dat$num_locats
+# dat$spend = as.numeric(dat$spend)/dat$num_locats
+# dat$spend_cad = as.numeric(dat$spend_cad)/dat$num_locats
+# dat$spend_eur = as.numeric(dat$spend_eur)/dat$num_locats
+# dat$spend_gbp = as.numeric(dat$spend_gbp)/dat$num_locats
+# 
+# dat$location_latitude = as.numeric(dat$location_latitude)
+# dat$location_longitude = as.numeric(dat$location_longitude)
+# wgs = subset(dat,
+#              location_latitude >= -180 &
+#                location_latitude <= 180 &
+#                location_longitude >= -180 &
+#                location_longitude <= 180
+#              )
+# nonwgs = subset(dat,
+#                 location_latitude < -180 |
+#                   location_latitude > 180 |
+#                   location_longitude < -180 |
+#                   location_longitude > 180
+# )
+# # NL accidentally forgot decimal points
+# # http://d-portal.org/q.xml?aid=NL-KVK-41009723-AFG228-1
+# nonwgs$location_latitude = nonwgs$location_latitude/10000
+# nonwgs$location_longitude = nonwgs$location_longitude/10000
+# 
+# dat = rbind(wgs,nonwgs)
+# 
+# save(dat, file="d_portal_map.RData")
+load("d_portal_map.RData")
 
 calculate_sectors = function(d_port_url){
   aid_id = substr(d_port_url,32,nchar(d_port_url))
-  xml_url = paste0("http://d-portal.org/q.xml?aid=",aid_id)
+  xml_url = URLencode(paste0("http://d-portal.org/q.xml?aid=",aid_id))
   xml_root = read_xml(xml_url)
   activity = xml_children(xml_root)[1]
   activity_children = xml_children(activity)
@@ -120,6 +122,7 @@ for(i in 1:length(unique.activities)){
       purpose_percentage=sec_vec,
       aid = unique.activity
     )
+    sec_df_tmp = data.table(sec_df_tmp)[,.(purpose_percentage=sum(purpose_percentage,na.rm=T)),by=.(purpose_code,aid)]
     sec_df_list[[sec_df_index]] = sec_df_tmp
     sec_df_index = sec_df_index + 1
   }
